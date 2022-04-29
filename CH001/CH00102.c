3b54ed2f84697eef004f2e12b43d2de06e4964af
Li <kui2.li@aptiv.com>
2022-04-29 14:55:13 +0800

    update by likui

---
+++ CH001/CH00102.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * C语言中的数据类型
+ * 
+ **/
+
+
+
+int main(int argc, char const *argv[])
+{
+
+    float f,x =3.6,y =5.2;
+
+    int i =4,a,b;
+
+    a = x+y;
+    b = (int)(x+y);
+
+    f = 10/i;
+
+    printf("a = %d,b = %d,f = %f,x = %f\n",a,b,f,x);
+
+    
+    return 0;
+}
