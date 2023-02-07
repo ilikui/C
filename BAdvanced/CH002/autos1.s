83e11af657776d683b33deee7989baffb509ff85
= <kui2.li@aptiv.com>
2023-02-07 13:24:20 +0800

    update by likui

---
+++ BAdvanced/CH002/autos1.s
@@ -0,0 +1,52 @@
+	.file	"autos1.c"
+	.text
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+LC0:
+	.ascii "abcdef\0"
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB13:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$16, %esp
+	call	___main
+	movl	$LC0, (%esp)
+	call	_myCode_PrintFun
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE13:
+	.globl	_myCode_PrintFun
+	.def	_myCode_PrintFun;	.scl	2;	.type	32;	.endef
+_myCode_PrintFun:
+LFB14:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	8(%ebp), %eax
+	movl	%eax, (%esp)
+	call	_puts
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE14:
+	.ident	"GCC: (MinGW.org GCC Build-2) 9.2.0"
+	.def	_puts;	.scl	2;	.type	32;	.endef
