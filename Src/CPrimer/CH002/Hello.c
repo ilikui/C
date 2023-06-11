c6b767b0b89400a03d263941f3d081cc4a41e5d6
ilikui <2563043733@qq.com>
2023-06-11 19:39:18 +0800

    update by likui

---
+++ Src/CPrimer/CH002/Hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * @brief  This is a demo for Learing C program
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+
+int main(int argc, char const *argv[])
+{
+    
+
+    printf("Hello World\n");
+    
+    return 0;
+}
