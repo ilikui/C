a3bcb686ec9220e6b23cf050a1b8aba44abdcd43
ilikui <2563043733@qq.com>
2023-06-12 20:41:04 +0800

    update by likui

---
+++ Src/CPrimer/CH003/Beep.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * @brief Beep 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+
+int main(int argc, char const *argv[])
+{
+    char beep  = '\007';
+
+    printf("\a");
+    return 0;
+}
