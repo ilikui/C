2bac7626de14548a45c5e13633d880b50b7a04d6
ilikui <2563043733@qq.com>
2024-03-31 09:50:48 +0800

    update by likui

---
+++ Src/CPrimer/CH002/printf.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+/**
+ * @brief  This is a Commnts
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char const *argv[])
+{
+    printf("output");
+    return 0;
+}
