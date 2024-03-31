7250b2eb2f54a5f6dc21abb6a40e540e2f4ebe91
ilikui <2563043733@qq.com>
2024-03-31 10:23:46 +0800

    update by likui

---
+++ Src/CPrimer/CH002/Demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    int a =3;
+    float x =12.3;
+
+    printf("a =%d\n",a);
+    printf("x = %f\n",x);
+    printf("a = 0x%b\n",a);
+    printf("Address a = %p",&a);
+    return 0;
+}
---
+++ Src/CPrimer/CH002/Demo_Print.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * @brief This is a demo 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char const *argv[])
+{
+    int data1 =10;
+    int data2 =5;
+    int data3;
+    printf("This is a test sw\n");
+    printf("data1 = %d,data2=%d\n",data1,data2);
+    data3 = data1 -data2;
+    printf("data3 = %d\n",data3);
+    return 0;
+}
