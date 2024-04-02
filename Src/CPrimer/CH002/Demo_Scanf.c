9d9af0e12b5f2ca04553398cca52ed594df2fadf
ilikui <2563043733@qq.com>
2024-04-02 20:33:10 +0800

    update by likui

---
+++ Src/CPrimer/CH002/Demo_Scanf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * @brief Scanf的使用
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char const *argv[])
+{
+    int data1;
+    int data2;
+    int data3;
+
+    printf("Pls enter a num: ");
+    scanf("%d",&data1);
+    printf("Enter End\n");
+    printf("data1 = %d",data1);
+
+
+    return 0;
+}
